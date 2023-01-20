c6ffa79d333af42cffdcd66b40db0d308370b21f
ievavaiti123 <110530476+ievavaiti123@users.noreply.github.com>
2023-01-20 16:00:13 +0000

    added osc

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -3,6 +3,7 @@
 function setup() {
   createCanvas(1000, 600);
 
+  osc = new p5.Oscillator('sine');
  
   
 }
@@ -11,4 +12,9 @@ function draw() {
   background(220);
   fill(236, 199, 21);
   circle(width/2, height/2, 30);
+}
+
+function playOscillator() {
+  osc.start();
+  playing = true;
 }
