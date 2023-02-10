d3d9a11bac9bee0fecf8b640691c95e5db264ad2
samuelealbani <115192228+samuelealbani@users.noreply.github.com>
2023-02-10 14:24:34 +0000

    added sound interaction

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -5,18 +5,35 @@ let canvas2;
 
 let bckColA, bckColB, currentBckColor;
 
+let mainOsc, amModOsc;
+
 function setup() {
   createCanvas(1000, 600);
 
   canvas2 = createGraphics(1000, 600);
 
-  pos = createVector(width/2, height/2);
-  speed = createVector(random(2), random(3,5));
+  pos = createVector(width / 2, height / 2);
+  speed = createVector(random(2), random(3, 5));
 
-  
+
   bckColA = color(255, 0, 155);
   bckColB = color(140, 20, 255);
   currentBckColor = bckColA;
+
+  // sound initialisation
+  amModOsc = new p5.Oscillator();
+  amModOsc.freq(4);
+  amModOsc.add(1);
+  amModOsc.amp(0.8);
+  amModOsc.disconnect(); // disconnect from audio output
+  amModOsc.start();
+
+  mainOsc = new p5.Oscillator();
+  mainOsc.setType('sine');
+  mainOsc.amp(0.2);
+  mainOsc.amp(amModOsc);
+  mainOsc.freq(440);
+  //mainOsc.start();
 }
 
 function draw() {
@@ -26,36 +43,46 @@ function draw() {
   move();
   checkEdges();
 
-
   textSize(20);
-  text("I've started a text poem", pos.x+30, pos.y);
+  text("I've started a text poem", pos.x + 30, pos.y);
 
   image(canvas2, 0, 0);
   if (mouseIsPressed) {
     canvas2.fill(236, 199, 21)
     canvas2.textSize(10);
     canvas2.text("It's up to you where to take it next", mouseX, mouseY);
+
+    let freq = map(mouseY, 0, height, 1000, 100);
+    mainOsc.freq(freq);
+    let amFreq = map(mouseX, 0, width, 2, 100);
+    amModOsc.freq(amFreq);
+
+
   } else {
     canvas2.clear()
     canvas2.fill(236, 199, 21)
     canvas2.textSize(20)
     canvas2.text("click mouse to reveal secret message", mouseX, mouseY)
   }
-  
+}
 
-  
-  
+function mousePressed() {
+  mainOsc.start();
+
+}
+
+function mouseReleased() {
+  mainOsc.stop();
 }
 
 
-
-function move(){
+function move() {
   pos.add(speed);
 }
 
-function changeStage(){
+function changeStage() {
   stage = !stage;
-  if(stage){
+  if (stage) {
     currentBckColor = bckColA;
   } else {
     currentBckColor = bckColB;
